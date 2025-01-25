5532984c14ce824b1cfe13d3d6480dffb60d30a4
riccardo-girone <149433192+riccardo-girone@users.noreply.github.com>
2025-01-25 17:04:44 +0100

    feat: created navbar + links

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,29 @@
+<div class="navContainer">
+	<div class="logo">
+		<div>logo</div>
+	</div>
+
+	<div class="linkContainer">
+		<div>tiket</div>
+		<div>log in/out</div>
+	</div>
+</div>
+
+<style>
+	.navContainer {
+		height: 4vw;
+		background-color: red;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		padding-left: 1vw;
+		padding-right: 1vw;
+	}
+
+	.linkContainer {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		gap: 1vw;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import Navbar from "$lib/components/navbar.svelte";
+</script>
+
+<Navbar />
+
+<style>
+	@import url("https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap");
+
+	:global(body) {
+		margin: 0;
+		font-family: "Montserrat", serif;
+	}
+</style>
